d26d55e619a17c7d261f9d5ff854aba5c4eae3a1
lucas0730 <lucas0730@naver.com>
2019-08-09 14:44:20 +0900

    Express Core : Middlewares part 2

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import express from "express";
-//const express = require("express");
+import express from "express"; //const express = require("express");
+import morgan from "morgan";
+import helmet from "helmet"; // 보안
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -12,18 +16,24 @@ const handleHome = (req, res) => res.send("Hello from home!!");
 
 const handleProfile = (req, res) => res.send("You are on my profile123");
 
-// middleware
-const betweenHome = (req, rest, next) => {
-  console.log("Between");
-  next();
+// middle ware
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+/*
+// 연결 끊기
+const middleware = (req, res, next) => {
+  res.send("not happening");
 };
-
-// 모든 웹 사이트에 적용될 middleware
-app.use(betweenHome); // global
-
+*/
 ////////////////////////////////
 
-app.get("/", betweenHome, handleHome);
+// app.get("/", middleware, handleHome);
+
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
