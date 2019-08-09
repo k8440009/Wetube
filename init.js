2570e8b96da989d619e2228fc4a74130a7ccf65e
lucas0730 <lucas0730@naver.com>
2019-08-09 18:38:34 +0900

    MVC Pattern part 1

--- init.js
+++ init.js
@@ -6,3 +6,7 @@ const handleListening = () =>
   console.log(`✅  Listeing on : http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
+
+// M : data -> DB
+// V : how does the data look -> template
+// C : fuction that looks for the data
