b8b534d865c78ed7d1dff20eaf0378aaab889136
lucas0730 <lucas0730@naver.com>
2019-08-09 20:09:38 +0900

    MVC Pattern part 3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import morgan from "morgan";
+import morgan from "morgan"; // logging
 import helmet from "helmet"; // 보안
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
