c3d262a79ef6c91ac6e2c2ab358d87f2858cebd1
lucas0730 <lucas0730@naver.com>
2019-08-30 02:09:27 +0900

    Adding FullScreen / ExitFullScreen

--- README.md
+++ README.md
@@ -23,12 +23,11 @@ Cloning Youtube with Vanilla and NodeJS
   3. 비디오 수정 추가 [x]
 - [] Video Player
   1. scss 추가 [x]
-  2. 실행/멈춤 [X]
-  3. 실행/멈춤시 i 태그 추가
-  4. 음소거
-  5. 풀 스크린
-  6. 현재시간/전체시간
-  7. 볼륨바
+  2. 실행/멈춤 [x]
+  3. 음소거 [x]
+  4. 풀 스크린 [x]
+  5. 현재시간/전체시간 []
+  6. 볼륨바 []
 
 ## MiddleWare
 
--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,7 +2,9 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayBtn");
 const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScrnBtn = document.getElementById("jsFullScreen");
 
+// Play 버튼
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
@@ -12,7 +14,7 @@ function handlePlayClick() {
     playBtn.innerHTML = '<i class="fas fa-play"></i>';
   }
 }
-
+// 볼륨 버튼
 function handleVolumeClick() {
   if (videoPlayer.muted) {
     videoPlayer.muted = false;
@@ -22,10 +24,24 @@ function handleVolumeClick() {
     volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 }
+// 풀 스크린, 작은 스크린
+function goFullScreen() {
+  videoContainer.webkitRequestFullscreen();
+  fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScrnBtn.removeEventListener("click", goFullScreen);
+  fullScrnBtn.addEventListener("click", exitFullScreen);
+}
+
+function exitFullScreen() {
+  fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScrnBtn.addEventListener("click", goFullScreen);
+  document.webkitExitFullscreen();
+}
 
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
+  fullScrnBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
